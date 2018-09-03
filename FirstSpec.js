d10f84c78aba59c8e9e6755de5bee74cf0b50536
Silioni Evi <SilioniE@unisystems.gr>
2018-09-03 17:59:33 +0300

    My first commit

---
+++ FirstSpec.js
@@ -0,0 +1,8 @@
+// Create First spec in Protractor
+
+describe("Login Functionality", function ()
+    {
+        it('Login Functionality with valid credentials', function () {
+            console.log("This is My test case");
+        });
+    });
